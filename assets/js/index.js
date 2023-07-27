61d7f6480cddbf90499d37fc11739711d34430db
MasterSawXDD <62162808+MasterSawXDD@users.noreply.github.com>
2023-07-27 14:22:59 -0400

    books page updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,32 @@
-
 const navList = document.getElementById("_navList");
 const btnToggle = document.getElementById("_toggle");
+const btnCategoriesToggle = document.getElementById("_categoriestoggle");
+const categoriesBody = document.getElementById("categoriesBody");
+
+function toggleElement(element) {
+  element.classList.toggle("open");
+  element.classList.toggle("close");
+}
 
 function disableScroll() {
-    document.body.style.overflow = "hidden";
-  }
+  document.body.style.overflow = "hidden";
+}
 
-  function enableScroll() {
-    document.body.style.overflow = "auto";
-  }
-  
+function enableScroll() {
+  document.body.style.overflow = "auto";
+}
 
 btnToggle.addEventListener('click', () => {
-   navList.classList.toggle("open");
-   btnToggle.classList.toggle("close");
+  toggleElement(navList);
+  toggleElement(btnToggle);
 
   navList.classList.contains('open') ? disableScroll() : enableScroll(); 
-
 });
+
+btnCategoriesToggle.addEventListener('click', () => {
+  toggleElement(categoriesBody);
+  toggleElement(btnCategoriesToggle);
+});
+
+
+
